f6961c70978974676b69569136a9c12418e38368
erikgomez <erikgomezc2003@gmail.com>
2024-11-26 23:30:37 +0100

    version 1.0

--- app/src/main/java/com/example/coinnect/MainActivity.kt
+++ app/src/main/java/com/example/coinnect/MainActivity.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
